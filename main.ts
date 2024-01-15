2e051c4d45c8f8843b8bb8a218d529970885cb6d
Alex Ramirez <alexis.rj2110@gmail.com>
2024-01-14 22:03:19 -0500

    Avance3

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import {map, filter, of} from "rxjs";
+import {filter, map, of} from "rxjs";
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+console.log("Aasd")
 
 of(1, 2, 3).subscribe(p => console.log(p))
 
@@ -30,24 +32,3 @@ class Cart {
     this.products.push(product);
   }
 }
-
-const products$ = of(
-  new Product("1","Product1",20),
-  new Product("2","Product2",10),
-  new Product("3","Product3",12),
-  new Product("4","Product4",16),
-  new Product("5","Product5",9),
-)
-
-const filtered$ = products$.pipe(
-
-  filter(p => p.price >= 12),
-  map(p => p.name)
-)
-
-filtered$.subscribe(p => {
-
-  let cart = new Cart();
-  cart.addProduct(p);
-  console.log(p)
-})
