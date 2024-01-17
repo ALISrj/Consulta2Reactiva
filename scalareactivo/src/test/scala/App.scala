fb98228dfb8c7e294a73342f1971d77f422d8cd7
Alex Ramirez <alexis.rj2110@gmail.com>
2024-01-17 16:10:53 -0500

    Avance 4

---
+++ scalareactivo/src/test/scala/App.scala
@@ -0,0 +1,31 @@
+import io.reactivex.Observable
+import io.reactivex.*
+import java.util.concurrent.TimeUnit
+
+
+object App {
+
+  @main
+  def reactivo() =
+
+    case class Product(id:String, name:String,price:Double) // Creamos clase de productos
+
+    val products: Observable[Product] = Observable.just(
+
+      Product("1", "Chocolate", 20.0),
+      Product("2", "Papa", 10.0),
+      Product("3", "Zanahoria", 12.0),
+      Product("4", "Leche", 16.0),
+      Product("5", "Lechuga", 9.0)) // Creamos un observable de productos
+
+    val filtrados = products
+      .delay(3, TimeUnit.SECONDS) // Delay emite los valores 3 segundo más tarde
+      .filter(_.price >= 12.0) // Filtramos por el precio
+      .map(_.name) // Obtenemos solo los nombrez
+
+    filtrados.subscribe(println(_)) // Nos suscribimos a la emision de filtrados
+
+    Thread.sleep(4000) // Esperamos el Delay para eso suspendemos el hilo 4 sgundos
+
+
+}
